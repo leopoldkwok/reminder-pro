44bc1dbc176075d90ae2874b83fd87f3a6af01b4
leopoldkwok <leopoldkwok@gmail.com>
2020-06-04 12:03:06 +0100

    render reminders and styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,12 +16,26 @@ class App extends Component {
     this.props.addReminder(this.state.text);
   }
 
+  renderReminders() {
+    const { reminders } = this.props;
+    return (
+      <ul className="list-group col-sm-4">
+        {reminders.map((reminder) => {
+          return (
+            <li key={reminder.id} className="list-group-item">
+              <div>{reminder.text}</div>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+
   render() {
-    console.log("this.props", this.props);
     return (
       <div className="App">
         <div className="title">Reminder Pro</div>
-        <div className="form-inline">
+        <div className="form-inline reminder-form">
           <div className="form-group">
             <input
               className="form-control"
@@ -37,13 +51,13 @@ class App extends Component {
             Add Reminder
           </button>
         </div>
+        {this.renderReminders()}
       </div>
     );
   }
 }
 
 function mapStateToProps(state) {
-  console.log("state", state);
   return { reminders: state };
 }
 
