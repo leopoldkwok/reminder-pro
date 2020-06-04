7f01419a3c4ee271862cb97dd1e153ba7a704e1f
leopoldkwok <leopoldkwok@gmail.com>
2020-06-04 11:36:54 +0100

    add mapDispatch to props using shortcut

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addReminder } from "../actions";
+
 import "../App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+    };
+  }
+
+  addReminder() {
+    this.props.addReminder(this.state.text);
+  }
+
   render() {
     return (
       <div className="App">
         <div className="title">Reminder Pro</div>
         <div className="form-inline">
           <div className="form-group">
-            <input className="form-control" placeholder="I have to..." />
+            <input
+              className="form-control"
+              placeholder="I have to..."
+              onChange={(event) => this.setState({ text: event.target.value })}
+            />
           </div>
-          <button type="button" className="btn btn-success">
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={() => this.addReminder()}
+          >
             Add Reminder
           </button>
         </div>
@@ -19,4 +41,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(null, { addReminder })(App);
